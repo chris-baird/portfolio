135d04957e13459b2acd7bdd8b6fa192b7727fbd
Chris baird <chrisbaird05@icloud.com>
2020-06-24 12:14:17 -0700

    Added function to switch pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Home from './pages/home';
+import Experience from './pages/experience';
+import About from './pages/about';
+import Contact from './pages/contact';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+} from 'reactstrap';
 
-function App() {
+export default function App() {
+
+  // State for mobile nav menu
+  const [isOpen, setIsOpen] = useState(false);
+
+  // State for current page being displayed
+  const [currentPage, setCurrentPage] = useState('home')
+
+  // Function to change nav state
+  const toggle = () => setIsOpen(!isOpen);
+
+  // Takes in the currentPage from state and returns its Component.
+  const displayPage = (currentPage) => {
+    switch (currentPage) {
+      case 'home':
+        return <Home />;
+      case 'experience':
+        return <Experience />;
+      case 'about':
+        return <About />;
+      case 'contact':
+        return <Contact />;
+      default:
+        return <h1>Something Broke :-(</h1>
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <Navbar color="light" light expand="md">
+        <NavbarBrand href="#" onClick={() => setCurrentPage('home')}>Chris Baird</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="ml-auto" navbar>
+            <NavItem>
+              <NavLink href="#" onClick={() => setCurrentPage('home')}>Home</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="#" onClick={() => setCurrentPage('experience')}>Experience</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="#" onClick={() => setCurrentPage('about')}>About</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="#" onClick={() => setCurrentPage('contact')}>Contact</NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+      {
+        displayPage(currentPage)
+      }
+      <div class="fixed-bottom">Footer</div>
+    </div >
   );
 }
 
-export default App;
+
